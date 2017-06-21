f6880740fc9700faec1fce291dddb846c74520b4
Nsikan <nsikane@live.co.uk>
2017-06-21 13:01:52 +0100

    Added Pug Main Page & CSS Styles

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 /* eslint linebreak-style: ["error", "windows"]*/
 const express = require('express');
+const path = require('path');
 const ConstReqParser = require('./app/customReqParser.class').CustomReqParser;
 
 const myapp = express();
 
 // Initial Settings
+myapp.use('/public', express.static(path.join(__dirname, 'public')));
 myapp.set('port', (process.env.PORT || 5000));
+myapp.set('views', './views');
+myapp.set('view engine', 'pug');
+
+// Routing
+
 myapp.get('/', (req, res) => {
+  res.render('index');
+});
+
+myapp.get('/sabesquiensoy', (req, res) => {
   res.send(JSON.stringify(ConstReqParser.createFromReq(req)));
 });
 // Starting the application
