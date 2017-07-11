9d4b3ad12329cadda8f7194aa82984ab7d00e472
Nsikan <nsikane@live.co.uk>
2017-07-11 13:01:23 +0100

    Additional server functionality to retrieve location

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* eslint linebreak-style: ["error", "windows"]*/
 const express = require('express');
 const path = require('path');
+const http = require('http');
 const ConstReqParser = require('./app/customReqParser.class').CustomReqParser;
 
 const myapp = express();
@@ -19,6 +20,32 @@ myapp.get('/', (req, res) => {
 
 myapp.get('/sabesquiensoy', (req, res) => {
   res.send(JSON.stringify(ConstReqParser.createFromReq(req)));
+});
+
+myapp.get('/dondeestoy', (req, res) => {
+  const ipaddressdirty = ConstReqParser.createFromReq(req).ipaddress;
+  const ipPattern = /(\d+\.\d+\.\d+\.\d+)/g;
+  if(ipaddressdirty.match(ipPattern)){
+    const ipaddress = ipaddressdirty.match(ipPattern)[0];
+    const _options = {
+      hostname: 'http://ip-api.com',
+      path: `/json/${ipaddress}`,
+    };
+    const _serverReq = http.request(_options);
+    _serverReq.on('response', (response) => {
+      res.send(response.body);
+    });
+    _serverReq.end();
+
+  } else {
+    res.send('no ip address found');
+  }
+  
+  /*
+  let _serverReq = http.request(_options, ()=> {
+
+  });
+  */
 });
 // Starting the application
 myapp.listen(myapp.get('port'), () => {
